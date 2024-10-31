34788a1d50b296d2d2ee663745572f39d64c7530
Шичков Александр <ctorkey@gmail.com>
2024-10-31 14:21:24 +0300

    site is done(without @media)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Массив с путями к изображениям
+// BG-IMG
 
 function getRandomRotation() {
 	return Math.floor(Math.random() * 360) // Поворот от 0 до 360 градусов
@@ -34,3 +34,49 @@ function fillBackground() {
 // Запуск функции при загрузке страницы и при изменении размера окна
 window.addEventListener('load', fillBackground)
 window.addEventListener('resize', fillBackground)
+
+// FAQ
+
+const questions = document.querySelectorAll('.faq__question')
+const questionsBtns = document.querySelectorAll('.faq__question-btn')
+
+for (let i = 0; i < questions.length; i++) {
+	questions[i].style.maxHeight =
+		questions[i].querySelector('.faq__question-head').clientHeight + 40 + 'px'
+}
+window.addEventListener('resize', function () {
+	for (let i = 0; i < questions.length; i++) {
+		if (questionsBtns[i].style.transform != 'rotate(90deg)') {
+			questions[i].style.maxHeight =
+				questions[i].querySelector('.faq__question-head').clientHeight +
+				40 +
+				'px'
+		} else {
+			questions[i].style.maxHeight = questions[i].scrollHeight + 'px'
+		}
+	}
+})
+
+function questionToggle(index) {
+	const headHeight = questions[index].querySelector(
+		'.faq__question-head'
+	).clientHeight // Получаем высоту faq__question-head
+
+	if (questionsBtns[index].style.transform == 'rotate(90deg)') {
+		questionsBtns[index].style.transform = 'rotate(0deg)'
+		if (window.innerWidth <= 720) {
+			questions[index].style.maxHeight = headHeight + 40 + 'px'
+		} else {
+			questions[index].style.maxHeight = headHeight + 40 + 'px'
+		}
+	} else {
+		questionsBtns[index].style.transform = 'rotate(90deg)'
+		questions[index].style.maxHeight = questions[index].scrollHeight + 'px'
+	}
+}
+
+for (let i = 0; i < questionsBtns.length; i++) {
+	questionsBtns[i].addEventListener('click', () => {
+		questionToggle(i)
+	})
+}
